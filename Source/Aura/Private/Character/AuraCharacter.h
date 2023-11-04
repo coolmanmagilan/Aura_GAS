b4fb3c4b443238e184e18d32291d4e0e3d015015
coolmanmagilan <magilanv5@gmail.com>
2023-11-04 15:26:15 -0400

    base character

---
+++ Source/Aura/Private/Character/AuraCharacter.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/BaseCharacter.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AAuraCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AAuraCharacter();
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+};
