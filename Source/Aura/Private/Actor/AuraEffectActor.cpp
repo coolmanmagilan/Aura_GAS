58a74161e69c2972f5d7bbaf428a948ed8db531e
coolmanmagilan <magilanv5@gmail.com>
2023-11-06 15:11:09 -0500

    EffectActor

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/AuraEffectActor.h"
+#include "Components/SphereComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	//Struct will not update every frame
+	PrimaryActorTick.bCanEverTick = false; //if you do not need tick function, then disable tick itself. 
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	SetRootComponent(StaticMesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(StaticMesh);
+}
+
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/*called when something starts to overlaps this component, for example a player walking into a trigger.
+	  In this case, a Sphere Mesh overlapping the Actor Class*/
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+	/*AddDynamic calls for a dynamic multicast delegate*/
+}
+
+
+/*OnOverlap is used as a trigger when the character overlaps with the sphere-like Health Potion(Other Actor)*/
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{	
+	/*---------------------------------------------------------------------------------------------------------------------
+		UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass())
+		Our PlayerState is derived from IAbilitySystemInterface, which has a function GetAbilitySystemComponent()
+		So what we need to do to manipulate Health in AbilitySystem is Get the AttributeSet from AbilitySystemComponent
+		Returns a UAttributeSet, which is why we cast to a UAuraAttributeSet.
+		It also returns a const, which is why we define AuraAttributeSet as a const
+		*/
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{	
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+
+
+		//TODO: Change this to apply a Gameplay EFFect. For now, using const_cast as a hack
+		//We use const_cast because AuraAttributeSet is const meaning we cannot use it to manipulate variable
+		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.F);
+		//---------------------------------------------------------------------------------------------------------------------//
+
+
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+}
+
---
+++ Source/Aura/Private/Actor/AuraEffectActor.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAuraEffectActor();
+
+	/*TO learn about the paramaters of a function, click on it's defintion. 
+	If it does not provide the necessary defintion, then click defintion on it's type*/
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlapComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor,					//the other actor that overlapped the sphere component
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	//Mesh is a sphere-shape, not a sphere itself. So to simplify the capsule for physical collision, we use a sphere.
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+
+	//Effect Actors ca
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+};
