6604d66febb3632eec4d4762d37b37f6d0062639
coolmanmagilan <magilanv5@gmail.com>
2023-11-05 22:24:26 -0500

    Attributes

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,63 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,
+		Health,										//it's going to replicate Health from UAuraAttributeSet
+		COND_None,									//it's going to replicate without any conditions
+		REPNOTIFY_Always);							/*if the value is set on the server, replicate it
+													 and on the client, that value will be updates on set*/
+
+	/*Note: REPNOTIFY_ONCHANGE will only replicate the variable if the value of health on the server change
+			This is optimization. If you're setting it to the same value, then there's no need to replicate it*/	
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,
+		MaxHealth,										
+		COND_None,									
+		REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,
+		Mana,										
+		COND_None,									
+		REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,
+		MaxMana,										
+		COND_None,									
+		REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	/*updates our 'current value' to the server
+	the value will then come back to the client and change if valid
+	Note: The ability system will register tha change until it is valid
+	If not valid, the server can roll back that change*/
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,53 @@ UCLASS()
 class UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UAuraAttributeSet();
+
+	/*
+	--------------------------------------------------------------------------------------------------
+	Returns the properties used for network replication his needs to be overridden by all actor classes 
+	with native replicated properties
+	*/
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const;
+	/*
+	Replicating properties means that changes made to these properties on one client are sent to the server
+	and then relayed to all other clients, ensuring that all players see the same state of the game
 	
+
+	Furthermore you must override this function in your class to specify which properties should be replicated.
+	-------------------------------------------------------------------------------------------------------------
+	*/
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	//Called when a new Health value arrives over the network
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+
+protected:
+
+private:
+
 };
